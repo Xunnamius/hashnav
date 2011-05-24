981f3d1a7efc635fd222b9b4f9a1ee377394a785
Tre Giles <trefighter2334@aol.com>
2011-05-24 02:23:23 -0500

    alpha update to 1.0!

---
+++ Source/HashNav-UNCOMPRESSED/HashNav.History.js
@@ -0,0 +1,84 @@
+/*
+---
+description: Addition of the history object and its methods to the HashNav object. Requires the HashNav core to function.
+
+license: MIT-style license
+
+authors:
+- Xunnamius
+
+requires:
+- HashNav/core
+
+provides: [HashNav.history]
+...
+*/
+
+/* documentation and updates @ http://github.com/Xunnamius/HashNav */
+(function() // Private
+{
+	var history = [];
+	
+	/* Public methods */
+	HashNav.implement('history',
+	{
+		get: function(entry)
+		{
+			if(entry == 'all') return Array.clone(history).each(function(item, i){ if(parseInt(item)+1) this[i] = this[item]; }); // Reversse the internal history optimization for the cloned history object
+			
+			entry = parseInt(entry),
+			negen = history.length + entry;
+			
+			if(entry >= 0) negen = history[entry];
+			else if(negen < history.length && negen >= 0) negen = history[negen];
+			else negen = null;
+			
+			if(negen && typeof(negen) == 'object') return negen;
+			if(parseInt(negen)+1) return history[negen];
+			else return false;
+		},
+		
+		truncate: function(count)
+		{
+			if(count >= history.length) return instance.history.clear();
+			else if(count > 0)
+			{
+				history.splice(0, count);
+				return true;
+			}
+			
+			else return false;
+		},
+		
+		clear: function()
+		{
+			history = [];
+			return true;
+		}
+	});
+	
+	/* "Private" utility methods */
+	HashNav.implement({
+		
+		$_hidden_history_loaded: true,
+		push: function(lochash)
+		{
+			history.every(function(item, i)
+			{
+				if(item[0] == lochash[0])
+				{
+					lochash = i;
+					return false;
+				}
+				
+				return true;
+			}.bind(this));
+			
+			history.push(lochash);
+		}.protect(),
+		
+		replace: function(newhist)
+		{ history = newhist; }.protect()
+		
+	});
+})();
