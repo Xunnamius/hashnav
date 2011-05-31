f1e8ced840953b15488cca8be150010136f29cf3
Tre Giles <trefighter2334@aol.com>
2011-05-30 17:14:06 -0700

    Edited Source/HashNav/String.QueryStringImproved.js via GitHub

--- Source/HashNav/String.QueryStringImproved.js
+++ Source/HashNav/String.QueryStringImproved.js
@@ -8,7 +8,7 @@ authors:
 - Xunnamius
 
 requires:
-- more/1.3.0.1: [String.QueryString]
+- more/1.3: [String.QueryString]
 
 provides: [String.parseQueryStringImproved, String.parseObjectToQueryString]
 ...
